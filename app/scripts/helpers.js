4704a0d5672edb95331548c621dbc6370d49a150
David Smith <davidsmith2@gmail.com>
2017-02-26 22:36:48 -0500

    Create unit tests for helpers

--- app/scripts/helpers.js
+++ app/scripts/helpers.js
@@ -4,20 +4,28 @@ define([
 ],
 function(Entities) {
 
-  Handlebars.registerHelper('renderStars', function(starRating) {
+  var renderStars = function(starRating) {
     var ret = '';
     _.each(new Array(starRating), function() {
       var model = new Entities.StarModel({name: 'off'});
       ret += '<img class="' + model.get('className') + '" src="' + model.get('src') + '">';
     });
     return ret;
-  });
+  };
 
-  Handlebars.registerHelper('reRenderStars', function($elements, starRating) {
+  var reRenderStars = function($elements, starRating) {
     _.each($elements, function(element, index) {
       var model = new Entities.StarModel({name: (starRating <= index) ? 'off' : 'on'});
       $(element).attr('src', model.get('src'));
     });
-  });
+  };
+
+  Handlebars.registerHelper('renderStars', renderStars);
+  Handlebars.registerHelper('reRenderStars', reRenderStars);
+
+  return {
+    renderStars: renderStars,
+    reRenderStars: reRenderStars
+  };
 
 });
---
+++ test/helpers.test.js
@@ -0,0 +1,35 @@
+define([
+  'helpers'
+],
+function(helpers) {
+
+  describe('helpers', function() {
+
+    describe('rendering stars', function() {
+
+      it('should output requisite number of images', function() {
+        var stars = helpers.renderStars(1);
+        expect(stars).toEqual('<img class="js-edit-review-star" src="images/star.png">');
+      });
+
+    });
+
+    describe('re-rendering stars', function() {
+
+      var getFilename = function(filepath) {
+        return filepath.replace(/^.*[\\\/]/, '');
+      };
+
+      it('should update requisite number of images', function() {
+        var $elements = $('<img src="images/star.png"><img src="images/star.png">');
+        var starRating = 1;
+        helpers.reRenderStars($elements, starRating);
+        expect(getFilename($elements.get(0).src)).toEqual('star-on.png');
+        expect(getFilename($elements.get(1).src)).toEqual('star.png');
+      });
+
+    });
+
+  });
+
+});
