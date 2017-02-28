48168e2fa5f8d8894360a78142dc411d75a4c916
David Smith <davidsmith2@gmail.com>
2017-02-27 19:00:45 -0500

    Fix user name/avatar alignment (fixes #4)

--- docs/scripts/templates.js
+++ docs/scripts/templates.js
@@ -6,7 +6,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   
 
 
-  return "<div class=\"row\">\n    <div class=\"col-sm-2 col-sm-offset-2\">\n        <h2 class=\"margin-top-0\">Reviews</h2>\n    </div>\n    <div class=\"col-sm-4\">\n        <a class=\"js-create-review\" href=\"#\">Write a review for this product</a>\n    </div>\n</div>\n<div class=\"row\" id=\"product__reviews--collection\"></div>";
+  return "<div class=\"row\">\n    <div class=\"col-sm-4 col-sm-offset-2\">\n        <div class=\"pull-left\">\n            <h2 class=\"margin-top-0\">Reviews</h2>\n        </div>\n        <div class=\"pull-right\">\n            <a class=\"js-create-review\" href=\"#\">Write a review for this product</a>\n        </div>\n    </div>\n</div>\n<div class=\"row\" id=\"product__reviews--collection\"></div>";
   });
 
 this["JST"]["app/templates/modal.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
@@ -55,11 +55,11 @@ function program1(depth0,data) {
   return buffer;
   }
 
-  buffer += "<div class=\"col-sm-2\">\n    <div class=\"centered-image\">\n        <img src=\"images/"
+  buffer += "<div class=\"col-sm-2\">\n    <figure class=\"pull-right\">\n        <img src=\"images/"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.user)),stack1 == null || stack1 === false ? stack1 : stack1.avatar)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "\">\n        <span>"
+    + "\">\n        <figcaption>"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.user)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</span>\n    </div>\n</div>\n<div class=\"col-sm-8\">\n    <div class=\"clearfix\">\n        <div class=\"pull-left margin-right-1\">\n            <h3 class=\"margin-top-0\">";
+    + "</figcaption>\n    </figure>\n</div>\n<div class=\"col-sm-8\">\n    <div class=\"clearfix\">\n        <div class=\"pull-left margin-right-1\">\n            <h3 class=\"margin-top-0\">";
   if (stack2 = helpers.title) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
   else { stack2 = (depth0 && depth0.title); stack2 = typeof stack2 === functionType ? stack2.call(depth0, {hash:{},data:data}) : stack2; }
   buffer += escapeExpression(stack2)
